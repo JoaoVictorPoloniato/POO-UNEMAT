9671830cb2c38688d28cf6a8cbff137c0d747012
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-03-26 21:25:46 -0400

    UPDATE JAVA

--- JAVA/televisorEncapsulamento.java
+++ JAVA/televisorEncapsulamento.java
@@ -56,3 +56,18 @@ public class TelevisorEncapsula {
         }
     }
 }
+
+
+----------------------------------------------------------------------
+
+public class usaTelevisorEncapsulada {
+    public static void main(String[] args){
+        TelevisorEncapsula tv = new TelevisorEncapsula();
+        tv.setCanal(150);
+        tv.aumentarCanal();
+        tv.setVolume(3);
+        tv.diminuirVolume();
+        System.out.println(tv.getCanal());
+        System.out.println(tv.getVolume());
+    }
+}
